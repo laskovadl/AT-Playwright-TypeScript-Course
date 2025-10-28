388f0b5491c9528778a50783ce2356dc92d7e732
Desislava Laskova <desislava.laskova@gmail.com>
2025-10-28 22:22:00 +0200

    Async-await

--- mini-online-shop.ts
+++ mini-online-shop.ts
@@ -1,32 +1,31 @@
-// We are going to build a small TypeScript program that describes a mini online shop. The goal is to represent a customer, their shopping cart, and calculate the total price. 
- 
-// Steps 
+// We are going to build a small TypeScript program that describes a mini online shop. 
+// The goal is to represent a customer, their shopping cart, and calculate the total price. 
+ // Steps 
+
 // 1.Create an interface Product with the following fields: 
 // name: string  
 // price: number  
 // category: "food" | "clothes" | "electronics" 
-// 2.Create a type Category with the possible categories (food, clothes, electronics) 
-// 3.Create an interface Customer with the following fields: 
-// name: string   
-// email: string 
-// cart: Product[] — an array containing the products in the customer’s cart 
-// 4. Create two example products (e.g., an apple and a T-shirt). 
-// 5. Create one customer who has both products
-
-type Category = "food" | "clothes" | "electronics";
-
 interface Product {
     name: string;
     price: number;
     category: Category;
 };
 
+// 2.Create a type Category with the possible categories (food, clothes, electronics) 
+type Category = "food" | "clothes" | "electronics";
+
+// 3.Create an interface Customer with the following fields: 
+// name: string   
+// email: string 
+// cart: Product[] — an array containing the products in the customer’s cart 
 interface Customer {
     name: string;   
     email:string;
     cart: Product[];
 };
 
+// 4. Create two example products (e.g., an apple and a T-shirt). 
 const apple: Product = {
     name: "Apple", 
     price: 2, 
@@ -38,29 +37,43 @@ const tShirt: Product = {
     price: 20, 
     category: "clothes",
 };
-// add products for electronics
 
-const products: Product[] = [apple, tShirt];
+// Add products for electronics
+const phone: Product = {
+    name: "Nokia", 
+    price: 5, 
+    category: "electronics"
+};
 
+const products: Product[] = [apple, tShirt, phone];
+
+// 5. Create one customer who has both products
 const customer: Customer = {
     name: "Mladen Petrov",
     email: "mladen@mail.bg",
     cart: products,
 };
-// add second customer
 
-// add function  displayCart - 
+// Add second customer
+const secondCustomer: Customer = {
+    name: "Petia Petrova",
+    email: "petia@mail.bg",
+    cart: products,
+};
+
+// Add function displayCart - 
 // display cart owner, customer name and products
+// function displayCart(customer: Customer) {
 
-// add function filterByCategory - 
+// Add function filterByCategory - 
 // receive products and category as parameters 
 // and filter the products by given category
 
-// add function calculateTotalPrice - 
-// receive customer data and combine all products price
-//hint: customer.cart[0].price
-//hint: customer.cart[1].price
-//...
-//total = summ
+// // Add function calculateTotalPrice - 
+// // receive customer data and combine all products price
+// //hint: customer.cart[0].price
+// //hint: customer.cart[1].price
+// //...
+// //total = summ
 
 export {};
